6fe8670010b3b41df4681f0541693803f837d6f7
Brian McKenna <brianmckenna@gmail.com>
2019-10-07 14:50:14 -0400

    cleanup aisle 3

--- cognito-register.py
+++ cognito-register.py
@@ -6,9 +6,6 @@ from warrant import Cognito
 
 from settings import *
 
-#pool_id   = 'us-east-1_KeZCl0wwt'
-#client_id = '2lup6diqlr7usgsn26sqm9vd6c'
-
 name   = input('Your Name: ')
 email  = input('Your Email: ')
 secret = input('Code Sprint Secret: ')
